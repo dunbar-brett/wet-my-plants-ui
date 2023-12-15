07a91baedeb2efa0375e5b5cd1a556fd3249af7d
dunbar-brett <dunbar-brett>
2023-12-15 13:54:52 -0800

    used cursor ai to make login form more responsive. need to make it look better

--- app/ui/loginForm.tsx
+++ app/ui/loginForm.tsx
@@ -8,41 +8,93 @@ import { ArrowRightIcon } from '@heroicons/react/20/solid';
 
 export default function LoginForm() {
   return (
-    <form className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-500 px-6 pb-4 pt-8">
-        <h1 className="mb-3 text-2xl">
+    // <form className="space-y-3">
+    //   <div className="flex-1 rounded-lg bg-gray-500 px-6 pb-4 pt-8">
+    //     <h1 className="mb-3 text-2xl">
+    //       Please log in to continue.
+    //     </h1>
+    //     <div className="w-full">
+    //       <div>
+    //         <label
+    //           className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+    //           htmlFor="email"
+    //         >
+    //           Email
+    //         </label>
+    //         <div className="relative">
+    //           <input
+    //             className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+    //             id="email"
+    //             type="email"
+    //             name="email"
+    //             placeholder="Enter your email address"
+    //             required
+    //           />
+    //           <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+    //         </div>
+    //       </div>
+    //       <div className="mt-4">
+    //         <label
+    //           className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+    //           htmlFor="password"
+    //         >
+    //           Password
+    //         </label>
+    //         <div className="relative">
+    //           <input
+    //             className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+    //             id="password"
+    //             type="password"
+    //             name="password"
+    //             placeholder="Enter password"
+    //             required
+    //             minLength={6}
+    //           />
+    //           <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <LoginButton />
+    //     <div className="flex h-8 items-end space-x-1">
+    //       {/* Add form errors here */}
+    //     </div>
+    //   </div>
+    // </form>
+    <form className="space-y-3 md:space-y-5">
+      <div className="flex-1 rounded-lg bg-gray-500 p-4 md:p-8">
+        <h1 className="mb-3 text-xl md:text-2xl">
           Please log in to continue.
         </h1>
         <div className="w-full">
           <div>
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              className="mb-3 mt-5 block text-xs md:text-sm font-medium text-gray-900"
               htmlFor="email"
             >
               Email
             </label>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full rounded-md border border-gray-200 py-2 md:py-[9px] pl-4 md:pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="email"
                 type="email"
                 name="email"
                 placeholder="Enter your email address"
                 required
               />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <AtSymbolIcon className="pointer-events-none absolute left-2 md:left-3 top-1/2 h-[14px] md:h-[18px] w-[14px] md:w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
           <div className="mt-4">
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              className="mb-3 mt-5 block text-xs md:text-sm font-medium text-gray-900"
               htmlFor="password"
             >
               Password
             </label>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full rounded-md border border-gray-200 py-2 md:py-[9px] pl-4 md:pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="password"
                 type="password"
                 name="password"
@@ -50,7 +102,7 @@ export default function LoginForm() {
                 required
                 minLength={6}
               />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <KeyIcon className="pointer-events-none absolute left-2 md:left-3 top-1/2 h-[14px] md:h-[18px] w-[14px] md:w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
