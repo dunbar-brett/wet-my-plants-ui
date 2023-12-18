4be543db1b5da4ef7eea0a5002e077728b13e807
dunbar-brett <dunbar-brett>
2023-12-18 14:58:10 -0800

    updated the side nav to use the ui buttons. looks a bit nicer"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import { Montserrat } from 'next/font/google'
-import './globals.css'
-import SideNav from '@/app/ui/sidenav'
-import Image from 'next/image'
+import { Montserrat } from 'next/font/google';
+import './globals.css';
+import SideNav from '@/app/ui/sidenav';
+import Image from 'next/image';
 
 const monts = Montserrat({ subsets: ['latin'] })
 
@@ -16,7 +16,7 @@ export default function RootLayout({
         {/* <div className="w-full flex-none md:w-64">Side Nav goes here</div>
         <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div> */}
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-          <div className="w-full flex-none md:w-64">
+          <div className="w-full md:w-64">
             <SideNav />
           </div>
           <div className="flex-grow md:overflow-y-auto">
--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -1,42 +1,65 @@
+import Image from 'next/image'
 import Link from 'next/link';
 // import NavLinks from '@/app/ui/dashboard/nav-links';
-import { Bars3Icon, PowerIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon, Cog6ToothIcon, PowerIcon } from '@heroicons/react/24/outline';
+import { Button } from '@/app/ui/forms/button';
+
 
 export default function SideNav() {
   return (
     <>
-      <div className="block md:hidden text-white">
+      <div className="block md:hidden">
+        {/* TODO: add a < chevron for back button */}
+        {/* TODO: add a back button that only shows on some pages */}
         <Bars3Icon className="w-12 absolute right-0 top-0"/>
       </div>
-      <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-plant-green hidden md:block">
+      <div className="flex h-screen flex-col px-3 py-4 md:px-2 bg-plant-green text-white hidden md:block justify-between">
         <Link
-          className="mb-2 flex h-20 items-end justify-start rounded-md p-4 md:h-40"
+          className="mb-2 flex h-20 items-center justify-center rounded-md p-4"
           href="/"
         >
-          <div className="w-32 text-white md:w-40">
+          {/* <div className="w-32 md:w-40">
             Big Logo
-          </div>
+          </div> */}
+          <div className="flex md:overflow-y-auto">
+              <Image
+                src="/logo.svg"
+                alt="Wet My Plants Logo"
+                width={30}
+                height={24}
+                priority
+              ></Image>
+            </div>
         </Link>
-        <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <div className="flex flex-col flex-grow justify-between space-x-2 md:space-x-0 md:space-y-2">
           {/* <NavLinks /> */}
-          Nav Links Components
-          Sign Up
-          Sign In 
-          Plant List
-          Location list
-          Assign Guest
-          <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-          <form>
-            <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-              <PowerIcon className="w-6" />
-              <div className="hidden md:block">Account Settings</div>
-            </button>
-            <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-              <PowerIcon className="w-6" />
-              <div className="hidden md:block">Sign Out</div>
-            </button>
-          </form>
+          <Button className="flex h-[48px] w-full grow items-center justify-center gap-2">
+            <span>Sign Up</span>
+          </Button>
+          <Button className="flex h-[48px] w-full grow items-center justify-center gap-2">
+            <span>Sign In</span>
+          </Button>
+          <Button className="flex h-[48px] w-full grow items-center justify-center gap-2">
+            <span>Plants</span>
+          </Button>
+          <Button className="flex h-[48px] w-full grow items-center justify-center gap-2">
+            <span>Locations</span>
+          </Button>
+          <Button className="flex h-[48px] w-full grow items-center justify-center gap-2">
+            <span>Assign Guest</span>
+          </Button>
         </div>
+        <form className="flex-shrink">
+          {/* TODO: add user profile card */}
+          <Button className="flex h-[48px] w-full grow items-center justify-center gap-2">
+            <Cog6ToothIcon className="w-6" />
+            <span>Account Settings</span>
+          </Button>
+          <Button className="flex h-[48px] w-full grow items-center justify-center gap-2">
+            <PowerIcon className="w-6" />
+            <span>Sign Out</span>
+          </Button>
+        </form>
       </div>
     </>
   );
